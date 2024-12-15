9b2a4193f5605fc12bbffc998ff39e5d8cc0fdbe
Karthikeyan Rangaswamy <karthikeyan.rangaswamy@photon.com>
2024-12-15 19:14:08 +0530

    Angular Find and Replace

--- src/app/find-replace/find-replace.component.ts
+++ src/app/find-replace/find-replace.component.ts
@@ -11,7 +11,9 @@ import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular
 })
 export class FindReplaceComponent {
   findReplaceForm: FormGroup;
-  highlightedText: string = '';
+  showOriginalText: string = '';
+  showReplaceText:string = '';
+  updatedText:string = '';
   occurrences: string = '';
   textReplaced:boolean = false;
   constructor(private fb: FormBuilder) {
@@ -46,17 +48,19 @@ const search = this.findReplaceForm.get('searchText')?.value || '';
 const replace = this.findReplaceForm.get('replaceText')?.value || '';
 if (search) {
         const regex = new RegExp(search, 'g');
-        this.occurrences = `${(text.match(regex) || []).length} Replaced with ${replace}`;
-        this.highlightedText = text.replace(regex, (match:string) => `<mark class='highlight'>${match}</mark>`);
-        const updatedText = text.replace(regex, replace);
-        this.findReplaceForm.get('textArea')?.setValue(updatedText);
+        this.occurrences = `Occurences Found: ${(text.match(regex) || []).length} <br/> ${search} Replaced with ${replace}`;
+        this.showOriginalText = text.replace(regex, (match:string) => `<mark class='highlight'>${match}</mark>`);
+        this.showReplaceText = text.replace(regex, (match:string) => `<mark class='highlight'>${replace}</mark>`);
+        this.updatedText = text.replace(regex, replace);
+        this.findReplaceForm.get('textArea')?.setValue(this.updatedText);
         this.textReplaced = true;
 }
 else {
       this.occurrences = '0';
       this.textReplaced = true;
-      this.highlightedText = text;
-      }
+      this.showOriginalText = text;
+      this.showReplaceText = this.updatedText;
+  }
 }
 
 }
