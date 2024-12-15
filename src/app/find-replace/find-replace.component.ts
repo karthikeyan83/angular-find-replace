d2f66b2d134061c8eafc828c1564385ee043c2be
Karthikeyan Rangaswamy <karthikeyan.rangaswamy@photon.com>
2024-12-15 19:51:20 +0530

    Angular Find and Replace

--- src/app/find-replace/find-replace.component.ts
+++ src/app/find-replace/find-replace.component.ts
@@ -10,10 +10,14 @@ import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular
   styleUrl: './find-replace.component.scss'
 })
 export class FindReplaceComponent {
+  //Field Declarations
   findReplaceForm: FormGroup;
+
+  //Variables to show the Original Text and Replaced Text
   showOriginalText: string = '';
   showReplaceText:string = '';
-  updatedText:string = '';
+
+  //Variables to show the Number of Occurences and Text Replaced
   occurrences: string = '';
   textReplaced:boolean = false;
   constructor(private fb: FormBuilder) {
@@ -48,18 +52,18 @@ const search = this.findReplaceForm.get('searchText')?.value || '';
 const replace = this.findReplaceForm.get('replaceText')?.value || '';
 if (search) {
         const regex = new RegExp(search, 'g');
-        this.occurrences = `Occurences Found: ${(text.match(regex) || []).length} <br/> ${search} Replaced with ${replace}`;
+        const textcount = (text.match(regex) || []).length;
+        this.occurrences = "Occurences Found:" + (textcount > 0 ? `${textcount} <br/> ${search} Replaced with ${replace}`: ' 0 <br/> No Text Replaced');
         this.showOriginalText = text.replace(regex, (match:string) => `<mark class='highlight'>${match}</mark>`);
         this.showReplaceText = text.replace(regex, (match:string) => `<mark class='highlight'>${replace}</mark>`);
-        this.updatedText = text.replace(regex, replace);
-        this.findReplaceForm.get('textArea')?.setValue(this.updatedText);
+        const updatedText = text.replace(regex, replace);
+        this.findReplaceForm.get('textArea')?.setValue(updatedText);
         this.textReplaced = true;
 }
 else {
       this.occurrences = '0';
       this.textReplaced = true;
       this.showOriginalText = text;
-      this.showReplaceText = this.updatedText;
   }
 }
 
